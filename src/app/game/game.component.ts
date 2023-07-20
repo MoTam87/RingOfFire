4467cd5f77d895af2c04e0d7d8c26725283e0bf6
Your Name git config --global user.email <mohamedtam914@gmail.com>
2023-07-20 16:32:59 +0200

    update

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -45,7 +45,7 @@ export class GameComponent  {
 
       const itemColletion = collection(this.firestore, 'games')
       
-      this.game$ = collectionData(itemColletion, { idField: params['id']})
+      this.game$ = collectionData(itemColletion, { idField: 'dblhzpZzjobaZlFCQeyo'})
       
       this.game$.subscribe( game=> {
            console.log('game update', game)
